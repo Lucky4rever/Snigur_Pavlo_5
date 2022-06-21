f2a921e67b015359cba343d39d1b063d6a1c0594
Lucky4rever <107917444+Lucky4rever@users.noreply.github.com>
2022-06-21 12:21:33 +0300

    Add files via upload

---
+++ ОП2_Снігур/ОП2_2_Снігур/Trees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ОП2_2_Снігур
+{
+    public class Trees
+    {
+        public string Type { get; set; }
+        public int Age { get; set; }
+        public string Health { get; set; }
+        public bool Pests { get; set; }
+    }
+}
